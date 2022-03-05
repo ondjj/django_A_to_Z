3a4eb98fe4e6452780fe96d73175dc6982e9c746
Chang-Jun Jeon <86146128+ondjj@users.noreply.github.com>
2022-03-05 23:51:23 +0900

    포스트 목록 페이지에 카테고리 기능 추가하기

--- do_it_django_prj/settings.py
+++ do_it_django_prj/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
+
     'blog.apps.BlogConfig',
     'single_pages.apps.SinglePagesConfig',
 ]
