1b49c6b9d279ade4a445e12a17533676e267ce7a
Chang-Jun Jeon <86146128+ondjj@users.noreply.github.com>
2022-03-14 19:46:20 +0900

    도커에 gunicorn 적용하기

--- do_it_django_prj/settings.py
+++ do_it_django_prj/settings.py
@@ -136,6 +136,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '_static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
